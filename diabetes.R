62a14d86309accd507b2b8ace361d2923cdeaf0f
Trishala Dinesh Ghone <47306009+trishalaghone@users.noreply.github.com>
2019-02-08 15:54:03 -0500

    Add files via upload

---
+++ diabetes.R
@@ -0,0 +1,15 @@
+my_data <- read.csv("C:/Users/Trishala/Desktop/diabetes.csv")
+summary(my_data)
+#columns
+#PregnanciesNumber of times pregnant
+#GlucosePlasma glucose concentration a 2 hours in an oral glucose tolerance test
+#BloodPressureDiastolic blood pressure (mm Hg)
+#SkinThicknessTriceps skin fold thickness (mm)
+#Insulin2-Hour serum insulin (mu U/ml)
+#BMIBody mass index (weight in kg/(height in m)^2)
+#DiabetesPedigreeFunctionDiabetes pedigree function
+#AgeAge (years)
+#OutcomeClass variable (0 or 1) 268 of 768 are 1, the others are 0
+
+head(my_data)
+structure(my_data)
